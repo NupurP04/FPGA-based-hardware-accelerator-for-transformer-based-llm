ee7b309035c620967b207266c01226f63d9e4e83
Nupur Patil <116893293+NupurP04@users.noreply.github.com>
2025-05-09 14:43:57 +0530

    Add files via upload

---
+++ GELU_verilog_files/GELU_wrapper.v
@@ -0,0 +1,148 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+//Date        : Sat Apr  5 22:41:52 2025
+//Host        : lifam-OptiPlex-7060 running 64-bit Ubuntu 20.04.6 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    aresetn_0,
+    axis_aclk_0,
+    m_axis_0_tdata,
+    m_axis_0_tkeep,
+    m_axis_0_tlast,
+    m_axis_0_tready,
+    m_axis_0_tvalid,
+    s_axis_0_tdata,
+    s_axis_0_tkeep,
+    s_axis_0_tlast,
+    s_axis_0_tready,
+    s_axis_0_tvalid);
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  input aresetn_0;
+  input axis_aclk_0;
+  output [31:0]m_axis_0_tdata;
+  output [3:0]m_axis_0_tkeep;
+  output m_axis_0_tlast;
+  input m_axis_0_tready;
+  output m_axis_0_tvalid;
+  input [31:0]s_axis_0_tdata;
+  input [3:0]s_axis_0_tkeep;
+  input s_axis_0_tlast;
+  output s_axis_0_tready;
+  input s_axis_0_tvalid;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire aresetn_0;
+  wire axis_aclk_0;
+  wire [31:0]m_axis_0_tdata;
+  wire [3:0]m_axis_0_tkeep;
+  wire m_axis_0_tlast;
+  wire m_axis_0_tready;
+  wire m_axis_0_tvalid;
+  wire [31:0]s_axis_0_tdata;
+  wire [3:0]s_axis_0_tkeep;
+  wire s_axis_0_tlast;
+  wire s_axis_0_tready;
+  wire s_axis_0_tvalid;
+
+  design_1 design_1_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .aresetn_0(aresetn_0),
+        .axis_aclk_0(axis_aclk_0),
+        .m_axis_0_tdata(m_axis_0_tdata),
+        .m_axis_0_tkeep(m_axis_0_tkeep),
+        .m_axis_0_tlast(m_axis_0_tlast),
+        .m_axis_0_tready(m_axis_0_tready),
+        .m_axis_0_tvalid(m_axis_0_tvalid),
+        .s_axis_0_tdata(s_axis_0_tdata),
+        .s_axis_0_tkeep(s_axis_0_tkeep),
+        .s_axis_0_tlast(s_axis_0_tlast),
+        .s_axis_0_tready(s_axis_0_tready),
+        .s_axis_0_tvalid(s_axis_0_tvalid));
+endmodule
